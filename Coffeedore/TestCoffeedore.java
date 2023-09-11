4e4c84fb747ea2fbedc848e5ec60363fa0a12ef7
vcntu <vcngit@gmail.com>
2023-09-11 09:29:39 -0700

    coffee changes

--- Coffeedore/TestCoffeedore.java
+++ Coffeedore/TestCoffeedore.java
@@ -6,7 +6,7 @@ public class TestCoffeedore {
 
         coffeeKiosk.addMenuItem("bagelchange", 8.99);
         coffeeKiosk.addMenuItem("donuts", 4.99);
-        coffeeKiosk.addMenuItem("latte,change in main", 2.50);
+        coffeeKiosk.addMenuItem("latte,change in main again", 2.50);
         coffeeKiosk.addMenuItem("coffeemainchange", 0.23);
         coffeeKiosk.addMenuItem("tea", 1.20);
 
