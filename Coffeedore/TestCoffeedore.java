399f12a0ba20be0899e480276a7d665c62e85879
vcntu <vcngit@gmail.com>
2023-09-10 21:10:35 -0700

    added new changes

--- Coffeedore/TestCoffeedore.java
+++ Coffeedore/TestCoffeedore.java
@@ -4,7 +4,7 @@ public class TestCoffeedore {
     public static void main(String[] args) {
         CoffeeKiosk coffeeKiosk = new CoffeeKiosk();
 
-        coffeeKiosk.addMenuItem("bagel", 8.99);
+        coffeeKiosk.addMenuItem("bagelchange", 8.99);
         coffeeKiosk.addMenuItem("donut", 4.99);
         coffeeKiosk.addMenuItem("latte", 2.50);
         coffeeKiosk.addMenuItem("coffee", 0.23);
